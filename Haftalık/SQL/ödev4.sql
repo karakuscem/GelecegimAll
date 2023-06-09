07c9928a479be3bc6892b715d1c8a22e566ce679
karakuscem <karakuscemenes@gmail.com>
2023-06-09 20:09:01 +0300

    ödev 4 yap

---
+++ Haftalık/SQL/ödev4.sql
@@ -0,0 +1,10 @@
+-- film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
+SELECT DISTINCT replacement_cost FROM film;
+-- film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
+SELECT COUNT(DISTINCT replacement_cost) FROM film;
+-- film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+SELECT title FROM film WHERE title LIKE 'T%' AND rating = 'G';
+-- country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+SELECT country FROM country WHERE country LIKE '_____';
+-- city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
+SELECT city FROM city WHERE city ILIKE '%R';
