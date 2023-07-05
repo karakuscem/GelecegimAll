b269b1b375ba3c6a65416839e1bb0aef20451459
karakuscem <karakuscemenes@gmail.com>
2023-07-05 19:53:23 +0300

    fix

--- Haftalık/Java/find-power/src/App.java
+++ Haftalık/Java/find-power/src/App.java
@@ -1,21 +1,19 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        // Girilen sayının 4 ve 5'in kuvvetini bulan program
-        int num, i = 1;
+    public static void main(String[] args) {
+        int num;
 
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a number: ");
         num = input.nextInt();
 
-        while(i <= num)
-        {
-            if (i % 4 == 0)
-                System.out.println(i + " is a power of 4.");
-            else if (i % 5 == 0)
-                System.out.println(i + " is a power of 5.");
-            i++;
+        for (int i = 1; i <= num; i *= 4) {
+            System.out.println("4 " + i);
+        }
+
+        for (int i = 1; i <= num; i *= 5) {
+            System.out.println("5 " + i);
         }
 
         input.close();
